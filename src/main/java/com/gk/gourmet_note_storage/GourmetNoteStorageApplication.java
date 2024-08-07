cf9d02c41bcfed7d2ee8738b1699b49462185e5d
gk <iba3374@naver.com>
2024-08-07 17:54:04 +0900

    first commit

---
+++ src/main/java/com/gk/gourmet_note_storage/GourmetNoteStorageApplication.java
@@ -0,0 +1,13 @@
+package com.gk.gourmet_note_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetNoteStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetNoteStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gk/gourmet_note_storage/GourmetNoteStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.gourmet_note_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetNoteStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
