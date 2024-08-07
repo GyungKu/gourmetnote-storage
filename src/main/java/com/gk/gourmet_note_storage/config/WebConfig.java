13ebc704e03bfae94a60b25b8f52d0e3a21bf26b
gk <iba3374@naver.com>
2024-08-07 19:15:34 +0900

    feat: WebConfig @Configuration 어노테이션 추가

--- src/main/java/com/gk/gourmet_note_storage/config/WebConfig.java
+++ src/main/java/com/gk/gourmet_note_storage/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.gk.gourmet_note_storage.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.origins}")
